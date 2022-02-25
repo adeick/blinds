0df91f4aae66a15f1fd44cb46e792b1086ee0494
adeick <adeick@iastate.edu>
2022-02-25 16:58:29 -0600

    Update Responsive Styles

--- components/BlindSlider.js
+++ components/BlindSlider.js
@@ -5,31 +5,35 @@ import {
     SliderThumb,
     SliderMark,
     Box,
+    useBreakpointValue,
 } from '@chakra-ui/react'
 
 import { MdGraphicEq } from 'react-icons/md';
 
 const BlindSlider = (props) => {
-    return (
-        <>
-            <Box h={props.phone ? '4%' : '70%'} w={props.phone ? '70%' : '4%'}>
-                <Slider
-                    aria-label='slider-ex-3'
-                    defaultValue={60}
-                    orientation={props.phone ? "horizontal" : "vertical"} //breakpoint({ base: 'vertical', sm: 'vertical', md: 'vertical', lg: 'vertical' })}
-                    minH={props.phone ? '1px' : '100px'}
-                    minW={props.phone ? '32px' : '1px'}
-                >
-                    <SliderTrack bg='red.100'>
 
-                        <SliderFilledTrack bg='tomato' />
-                    </SliderTrack>
-                    <SliderThumb boxSize={6}>
-                        <Box color='tomato' as={MdGraphicEq} />
-                    </SliderThumb>
-                </Slider>
-            </Box >
-        </>
+    return (
+        <Box h={props.phone ? "4%" : "70%"} w={props.phone ? "70%" : "4%"}>
+            <Slider
+                aria-label='slider-ex-3'
+                defaultValue={60}
+                orientation={props.phone ? 'horizontal' : 'vertical'}
+                minH={props.phone ? "40px" : '100px'}
+                minW={props.phone ? '100px' : '10px'}
+            >
+                <SliderTrack bg='red.100'>
+
+                    <SliderFilledTrack bg='tomato' />
+                </SliderTrack>
+                <SliderThumb boxSize={6}>
+                    <Box
+                        color="tomato"
+                        as={MdGraphicEq}
+                    />
+                </SliderThumb>
+            </Slider>
+
+        </Box >
     )
 };
 
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import {
   Text,
   useColorMode,
   useColorModeValue,
-  VStack, Spacer,
+  VStack, Stack, Spacer,
   useBreakpointValue,
   HStack,
   Slider,
@@ -20,6 +20,8 @@ import {
   SliderThumb,
   SliderMark,
 } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+
 
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 import { FaMoon, FaCloudSun, FaCloud, FaSun } from 'react-icons/fa';
@@ -31,6 +33,26 @@ import BlindSlider from '../components/BlindSlider'
 
 const Home = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [windowSize, setWindowSize] = useState(0);
+
+  useEffect(() => {
+    setWindowSize(window.outerWidth);
+    let resize = function () { setWindowSize(window.outerWidth) };
+    window.addEventListener('resize', resize, false);
+    return (() => {
+      window.removeEventListener('resize', resize, false);
+    });
+  }, [windowSize]);
+
+  const bgHeight = useBreakpointValue({ base: '144vh', md: '94vh' });
+  const presetTextMargin = useBreakpointValue({ base: '1vh', md: "6vh" });
+  const rowColumn = useBreakpointValue({ base: 'row', md: 'column' });
+  const columnRow = useBreakpointValue({ base: 'column', md: 'row' });
+  const smallMedium = useBreakpointValue({ base: 'sm', md: 'md' });
+  const manualBoxHeight = useBreakpointValue({ base: '100vh', md: '50vh' });
+  const trueFalse = useBreakpointValue({ base: true, md: false });
+
+  const phone = windowSize < 768; //number should be adjusted
 
   return (
     <div id="Home">
@@ -38,92 +60,69 @@ const Home = () => {
         <IconButton size="md" mr="30px" aria-label='Search database' icon={useColorModeValue(<MoonIcon />, <SunIcon />)} onClick={toggleColorMode} />
         NavBar
       </Flex>
-      <Flex bg={useColorModeValue("gray.200", "gray.900")} h={useBreakpointValue({ base: '144vh', md: '94vh' })} w="100%" py="5vh">
+      <Flex bg={useColorModeValue("gray.200", "gray.900")} h={bgHeight} w="100%" py="5vh">
         <VStack spacing="5vh" w="100%">
-          <Flex bg={useColorModeValue("orange.100", "orange.200")} h="25vh" w="80vw" py="3vh" px="30px" borderRadius="15px" direction={useBreakpointValue({ base: 'row', md: 'column' })}>
-            <Text color="black" as="b" mb={useBreakpointValue({ base: '1vh', md: "6vh" })} position="absolute">
+          <Flex bg={'orange.200'} h="25vh" w="80vw" py="3vh" pr="80px" pl="30px" borderRadius="15px" direction={rowColumn}>
+            <Text color="black" as="b" mb={presetTextMargin} position="absolute">
               Presets
             </Text>
-            <Flex w="100%" h="100%" direction={useBreakpointValue({ base: 'column', md: "row" })} alignItems={useBreakpointValue({ base: 'center', md: 'flex-end' })}>
+            <Stack direction={['column', 'row']} w="100%" h="100%" alignItems='flex-end'>
               <Spacer />
-              <Button my="5px" minWidth="110px" variant="solid" size={useBreakpointValue({ base: 'sm', md: 'md' })}
+              <Button my="5px" w="110px" variant="solid" size={smallMedium}
                 bg="gray.900" _hover={{ bg: "gray.700" }} _active={{ bg: "gray.800" }} leftIcon={<Icon as={FaMoon} color="yellow.200" />}>
                 <Text color="white">
                   All Closed
                 </Text>
               </Button>
               <Spacer />
-              <Button my="5px" minWidth="110px" variant="solid" colorScheme='orange' size={useBreakpointValue({ base: 'sm', md: 'md' })}
+              <Button my="5px" w="110px" variant="solid" size={smallMedium}
                 bg="gray.600" _hover={{ bg: "gray.500" }} _active={{ bg: "gray.400" }} leftIcon={<Icon as={FaCloud} color="gray.300" />}>
                 <Text color="white">
                   All Dim
                 </Text>
               </Button>
               <Spacer />
-              <Button my="5px" minWidth="110px" variant="solid" colorScheme='orange' size={useBreakpointValue({ base: 'sm', md: 'md' })}
+              <Button my="5px" w="110px" variant="solid" size={smallMedium}
                 bg="gray.50" _hover={{ bg: "gray.200" }} _active={{ bg: "gray.100" }} leftIcon={<Icon as={FaSun} color="yellow.500" />}>
                 <Text color="black">
                   All Open
                 </Text>
               </Button>
               <Spacer />
-            </Flex>
+            </Stack>
           </Flex>
-          <Flex bg={useColorModeValue("orange.200", "orange.300")} direction="column" h={useBreakpointValue({ base: '100vh', md: '50vh' })} w="80vw" borderRadius="15px">
+          <Flex bg={useColorModeValue("orange.300", "orange.300")} direction="column" h={manualBoxHeight} w="80vw" borderRadius="15px">
             <Text pl="30px" py="3vh" color="black" as="b" position="absolute">
               Manual
             </Text>
             <Flex w="100%" h="100%" pt="8%"
-              direction={useBreakpointValue({ base: 'column', md: 'row' })}
+              direction={phone ? "column" : "row"}
               alignItems="center" pb="30px">
               <Spacer />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
               <Spacer />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
               <Spacer />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
               <Spacer />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
               <Spacer />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
-              <BlindSlider phone={useBreakpointValue({ base: true, md: false })} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
+              <BlindSlider phone={phone} />
               <Spacer />
             </Flex>
           </Flex>
         </VStack>
       </Flex >
-      {/* <Flex  flexDirection="column"
-      minHeight={window.innerHeight *1.2} 
-      w="85vw" right="0" position="absolute" className="Background" 
-      backgroundImage={
-        useColorModeValue(
-        `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='88' height='88' viewBox='0 0 88 88'%3E%3Cg fill='%234072fb' fill-opacity='0.09'%3E%3Cpath fill-rule='evenodd' d='M29.42 29.41c.36-.36.58-.85.58-1.4V0h-4v26H0v4h28c.55 0 1.05-.22 1.41-.58h.01zm0 29.18c.36.36.58.86.58 1.4V88h-4V62H0v-4h28c.56 0 1.05.22 1.41.58zm29.16 0c-.36.36-.58.85-.58 1.4V88h4V62h26v-4H60c-.55 0-1.05.22-1.41.58h-.01zM62 26V0h-4v28c0 .55.22 1.05.58 1.41.37.37.86.59 1.41.59H88v-4H62zM18 36c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H20a2 2 0 0 1-2-2zm0 16c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H20a2 2 0 0 1-2-2zm16-26a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zM34 58a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zM34 78a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-6zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-6zM34 4a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2V4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2V4zm-8 82a2 2 0 1 1 4 0v2h-4v-2zm0-68a2 2 0 1 1 4 0v10a2 2 0 1 1-4 0V18zM66 4a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0V4zm0 72a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0v-8zm-48 0a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0v-8zm0-72a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0V4zm24-4h4v2a2 2 0 1 1-4 0V0zm0 60a2 2 0 1 1 4 0v10a2 2 0 1 1-4 0V60zm14-24c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H58a2 2 0 0 1-2-2zm0 16c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H58a2 2 0 0 1-2-2zm-28-6a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm8 26a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM36 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm-8-8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 68a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16-34a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16-12a2 2 0 1 0 0 4 6 6 0 1 1 0 12 2 2 0 1 0 0 4 10 10 0 1 0 0-20zm-64 0a2 2 0 1 1 0 4 6 6 0 1 0 0 12 2 2 0 1 1 0 4 10 10 0 1 1 0-20zm56-12a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 48a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm-48 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0-48a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm24 32a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-4a6 6 0 1 0 0-12 6 6 0 0 0 0 12zm36-36a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM10 44c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm56 0c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm8 24c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zM3 68c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4H5a2 2 0 0 1-2-2zm0-48c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4H5a2 2 0 0 1-2-2zm71 0c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm6 66a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM8 86a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0-68A6 6 0 1 1 8 2a6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm36 36a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'/%3E%3C/g%3E%3C/svg%3E")`,
-        `url("data:image/svg+xml,%3Csvg width='80' height='88' viewBox='0 0 80 88' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M22 21.91V26h-2c-9.94 0-18 8.06-18 18 0 9.943 8.058 18 18 18h2v4.09c8.012.722 14.785 5.738 18 12.73 3.212-6.99 9.983-12.008 18-12.73V62h2c9.94 0 18-8.06 18-18 0-9.943-8.058-18-18-18h-2v-4.09c-8.012-.722-14.785-5.738-18-12.73-3.212 6.99-9.983 12.008-18 12.73zM54 58v4.696c-5.574 1.316-10.455 4.428-14 8.69-3.545-4.262-8.426-7.374-14-8.69V58h-5.993C12.27 58 6 51.734 6 44c0-7.732 6.275-14 14.007-14H26v-4.696c5.574-1.316 10.455-4.428 14-8.69 3.545 4.262 8.426 7.374 14 8.69V30h5.993C67.73 30 74 36.266 74 44c0 7.732-6.275 14-14.007 14H54zM42 88c0-9.94 8.06-18 18-18h2v-4.09c8.016-.722 14.787-5.738 18-12.73v7.434c-3.545 4.262-8.426 7.374-14 8.69V74h-5.993C52.275 74 46 80.268 46 88h-4zm-4 0c0-9.943-8.058-18-18-18h-2v-4.09c-8.012-.722-14.785-5.738-18-12.73v7.434c3.545 4.262 8.426 7.374 14 8.69V74h5.993C27.73 74 34 80.266 34 88h4zm4-88c0 9.943 8.058 18 18 18h2v4.09c8.012.722 14.785 5.738 18 12.73v-7.434c-3.545-4.262-8.426-7.374-14-8.69V14h-5.993C52.27 14 46 7.734 46 0h-4zM0 34.82c3.213-6.992 9.984-12.008 18-12.73V18h2c9.94 0 18-8.06 18-18h-4c0 7.732-6.275 14-14.007 14H14v4.696c-5.574 1.316-10.455 4.428-14 8.69v7.433z' fill='%23151313' fill-opacity='0.35' fill-rule='evenodd'/%3E%3C/svg%3E")`
-        )}
-      pt="4%">
-        <Box px={["20px", "20px", "40px", "80px"]} >
-        <VStack w="100%" h="100%" spacing="0px">
-        <Text as="b" fontSize={[30, 30, 50, 70]} w="100%" fontFamily="Lexend Deca" mb="20px">
-        Hello, I'm Andrew Deick!
-        </Text>
-        <Text fontSize={[25, 25, 35, 35]} fontFamily="Newsreader">
-          I am a Software Engineer at Iowa State, and love learning new skills. In the past, I've dabbled in Virtual Reality, 
-          Front-End Development, and Machine Learning. Right now, I'm psyched about space. Check out <Link href='./favorites'><a><Text as="u" fontFamily="Lexend Deca" color={useColorModeValue("green.500", "green.300")}>Favorites</Text></a></Link> for more!
-        </Text>
-        </VStack>
-        </Box>
-        <Spacer/>
-        <Footer text="Contact Me:"/>
-      </Flex> */}
     </div >
   )
 };
